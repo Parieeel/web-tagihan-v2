125b98e1c9a7e22cf10785c3ca5b954c6214a985
Parieeel <ferlandadinataa@gmail.com>
2026-02-12 20:52:52 +0700

    Add files via upload

--- web tagihan v2/app.py
+++ web tagihan v2/app.py
@@ -25,4 +25,5 @@ def tagihan():
     return render_template("tagihan.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
