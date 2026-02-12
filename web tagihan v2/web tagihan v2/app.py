2165ffc6f2254b52c644a1bf571e825aa837099c
Parieeel <ferlandadinataa@gmail.com>
2026-02-12 20:58:07 +0700

    Add files via upload

---
+++ web tagihan v2/web tagihan v2/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+@app.route("/main_menu")
+def main_menu():
+    return render_template("main_menu.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/laporan")
+def laporan():
+    return render_template("laporan.html")
+
+@app.route("/pelanggan")
+def pelanggan():
+    return render_template("pelanggan.html")
+
+@app.route("/tagihan")
+def tagihan():
+    return render_template("tagihan.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
